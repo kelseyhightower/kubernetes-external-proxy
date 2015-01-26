07f1e265ce887c7d352b75b05d9610575839ba14
Kelsey Hightower <kelsey.hightower@gmail.com>
2015-01-25 17:38:28 -0800

    breakup service manager and service proxy

--- service_proxy.go
+++ service_proxy.go
@@ -23,72 +23,6 @@ type Service struct {
 	Selector      map[string]string `json:"selector"`
 }
 
-type ServiceManager struct {
-	mu sync.Mutex
-	m  map[string]*ServiceProxy
-}
-
-func newServiceManager() *ServiceManager {
-	m := make(map[string]*ServiceProxy)
-	return &ServiceManager{m: m}
-}
-
-func (sm *ServiceManager) Add(args *Service, reply *string) error {
-	if err := sm.add(args); err != nil {
-		log.Println(err)
-		return err
-	}
-	*reply = net.JoinHostPort(bindIP, args.Port)
-	log.Println("new service added", args)
-	return nil
-}
-
-func (sm *ServiceManager) Del(args *string, reply *bool) error {
-	if err := sm.del(*args); err != nil {
-		log.Println(err)
-		return err
-	}
-	*reply = true
-	log.Println("service service deleted", args)
-	return nil
-}
-
-func (sm *ServiceManager) add(service *Service) error {
-	sm.mu.Lock()
-	defer sm.mu.Unlock()
-	if _, ok := sm.m[service.ID]; ok {
-		err := fmt.Errorf("service %s already exist", service.ID)
-		log.Println(err)
-		return err
-	}
-	sp := newServiceProxy(service)
-	err := sp.start()
-	if err != nil {
-		log.Printf("error adding service %s: %v\n", service.ID, err)
-		return err
-	}
-	sm.m[service.ID] = sp
-	return nil
-}
-
-func (sm *ServiceManager) del(id string) error {
-	sm.mu.Lock()
-	defer sm.mu.Unlock()
-	if v, ok := sm.m[id]; ok {
-		delete(sm.m, id)
-		if err := v.stop(); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (sp *ServiceProxy) stop() error {
-	sp.shutdown <- true
-	<-sp.done
-	return nil
-}
-
 type ServiceProxy struct {
 	service  *Service
 	shutdown chan bool
@@ -141,6 +75,12 @@ func (sp *ServiceProxy) start() error {
 			ln.Close()
 		}
 	}()
+	return nil
+}
+
+func (sp *ServiceProxy) stop() error {
+	sp.shutdown <- true
+	<-sp.done
 	return nil
 }
 
